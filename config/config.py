6685779c0017fc441be5d95a732aa4e1aab14a77
chanpatil <41775765+chanpatil@users.noreply.github.com>
2020-05-20 13:16:26 +0530

    updated the config with heroku app name
    updated the config with heroku app name

--- config/config.py
+++ config/config.py
@@ -18,9 +18,9 @@ resume_api = "https://apisalesdemo2.successfactors.eu/odata/v2/JobApplication(31
 jobdesc_api = "https://apisalesdemo2.successfactors.eu/odata/v2//JobRequisition('2942')?$format=json&$expand=jobReqLocale"
 
 #path ="D:/Final Resume Scoring/FinalResumeReaderApplication/word"
-path = "../ResumeScoring/word/"
+path = "../mtresumescoringapplication/word/"
 
-tempPath = "../ResumeScoring/temp/"
+tempPath = "../mtresumescoringapplication/temp/"
 
 #creds
 creds = {
