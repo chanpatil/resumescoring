c41ae225653b60da6ec4c4c6106470197e617321
chanpatil <41775765+chanpatil@users.noreply.github.com>
2020-05-20 14:21:52 +0530

    changed path and tempPath
    changed path and tempPath

--- config/config.py
+++ config/config.py
@@ -18,9 +18,9 @@ resume_api = "https://apisalesdemo2.successfactors.eu/odata/v2/JobApplication(31
 jobdesc_api = "https://apisalesdemo2.successfactors.eu/odata/v2//JobRequisition('2942')?$format=json&$expand=jobReqLocale"
 
 #path ="D:/Final Resume Scoring/FinalResumeReaderApplication/word"
-path = "../resumescoring/word/"
+path = "../app/word/"
 
-tempPath = "../resumescoring/temp/"
+tempPath = "../app/temp/"
 
 #creds
 creds = {
