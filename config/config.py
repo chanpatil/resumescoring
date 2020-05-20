a004e58b0c707cb518fd958f983b7738d48f2fde
chanpatil <41775765+chanpatil@users.noreply.github.com>
2020-05-20 12:07:56 +0530

    Updating import statement
    Updating import statement in a file

--- config/config.py
+++ config/config.py
@@ -5,6 +5,8 @@ Created on Mon May 11 15:07:45 2020
 @author: Chanabasagoudap
 """
 
+from werkzeug.security import generate_password_hash
+
 # HTTP Basic Authentication
 username = 'sfadmin@SFPART035062'
 password = 'Test@123'
